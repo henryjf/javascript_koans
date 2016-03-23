dbf78201fbf84897e2ff491dea751622a50e7bc7
JEFF HENRY <henry_jf@hotmail.com>
2016-03-23 05:45:42 -0400

    36

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -64,14 +64,16 @@ describe("About Higher Order Functions", function () {
     expect(_(mixedBag).all(isEven)).toBe(false);
   });
 
+  //TODO  WHY true/false
+
   it("should use 'any' to test if any items passes condition" , function () {
     var onlyEven = [2,4,6];
     var mixedBag = [2,4,5,6];
 
     var isEven = function(x) { return x % 2 === 0 };
 
-    expect(_(onlyEven).any(isEven)).toBe(FILL_ME_IN);
-    expect(_(mixedBag).any(isEven)).toBe(FILL_ME_IN);
+    expect(_(onlyEven).any(isEven)).toBe(true);
+    expect(_(mixedBag).any(isEven)).toBe(true);
   });
 
   it("should use range to generate an array", function() {
